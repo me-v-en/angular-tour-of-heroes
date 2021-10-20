272b1fec745ee66a6b5cc5c822be84d4c106eb40
Meven AMELINE <meven.ameline@gmail.com>
2021-10-20 07:58:17 +0200

    Style graphique de la liste de heros + ajout d'un composant de présentation des détails d'un héro

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,10 +9,15 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   };
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;    
+  }
 
 }
