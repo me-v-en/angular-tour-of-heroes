3af90acf6cab04481b260b6e7ed51a4ab2b72b57
Meven AMELINE <meven.ameline@gmail.com>
2021-10-20 07:44:31 +0200

    Liste de héros

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = HEROES;
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+  };
 
 }
